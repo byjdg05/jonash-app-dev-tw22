aa8d86ad1da5e6e6b96948bc8b362b3f4ae14dbc
nash <jonashdeguia13@gmail.com>
2026-05-19 10:58:27 +0800

    Delete s2technical directory

--- s2technical/problem3/objective.php
+++
@@ -1,6 +0,0 @@
-<h2>Career Objective</h2>
-<p style="line-height: 1.6; font-size: 0.95rem;">
-    Aspiring developer with a passion for learning web and application development, 
-    digital content creation, and online business management platforms. Seeking to enhance 
-    foundational technology structures while building production-ready projects.
-</p>
